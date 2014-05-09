d206aad06861b20d64f8d8fd17210382620e31a1
Seth Swenson <sswenson@genedx.com>
2014-05-09 17:19:19 -0400

    Create 14_independent_alleles

---
+++ lib/14_independent_alleles.rb
@@ -0,0 +1,5 @@
+def full_heterozygosity_probability(generation_number = 1, num_organisms_at_generation = 2)
+    raise "invalid number of organisms" if num_organisms_at_generation > 2 ** generation_number
+
+
+end
